d336ac4359bbfee7cc9b5a3dee36426fcd3cc0f9
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2025-12-03 01:46:31 +0530

    Initial commit to test websocket connection.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on("message", (data) => {
+        console.log("message received:", data);
+
+
+        socket.emit("reply", "Message received by server");
+    });
+});
+
+
+server.listen(3000, () => console.log("API connected on post 3000"))
+
+
+// ws://localhost:3000/socket.io/?EIO=4&transport=websocket
